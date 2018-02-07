b0873a8d0963e66644b8be3835e3cffe4574cf03
MarinusWA <mwa.vandewijdeven@gmail.com>
2018-02-07 12:58:54 +0100

    Added BittrexExchange.cs

---
+++ CSCEP/Exchanges/BittrexExchange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dauros.Timango.CSCEP
+{
+    public class BittrexExchange : CryptocoinExchange
+    {
+        #region Constructors
+        public BittrexExchange() : base() { ApiUrl = "https://api.kraken.com"; }
+        public BittrexExchange(String apiKey, String secretKey) : base(apiKey, secretKey) { ApiUrl = "https://api.kraken.com"; }
+        #endregion
+    }
+}
